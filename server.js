5f58fa7a4f8bb52349e2b90411a19bd947ade9c6
unknown <axxes8@gmail.com>
2022-05-04 13:12:11 -0600

    Fixed bugs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express"); //import library
-
+const port = 3000;
 const app = express(); //using library
 
-app.listen(3000, ()=>{console.log("listening...")});
+app.listen(port, ()=>{console.log("listening on port: " + port)});
 
 app.get('/',(request,response)=>{response.send("Hello")});
