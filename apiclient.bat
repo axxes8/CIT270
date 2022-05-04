535ab929f23d5d72fc97cc19ea21de3aa1c92c21
unknown <axxes8@gmail.com>
2022-05-04 13:43:44 -0600

    Broke some things

--- apiclient.bat
+++ apiclient.bat
@@ -1,1 +1,1 @@
-curl -d "@data.json" -X POST http://localhost/login
+curl -d "@data.json" -X POST http://localhost:3000/login
--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require("express"); //import library
+const bodyParser = require('body-parser'); //This is middleware
+const res = require("express/lib/response");
 const port = 3000;
 const app = express(); //using library
 
+app.use(bodyParser.json()); //use the middleware, (call it before anything eles happens on each request)
+
 app.listen(port, ()=>{console.log("listening on port: " + port)});
 
-app.get('/',(request,response)=>{response.send("Hello")});
+app.get('/',(request,response)=>{//Everytime something call the API this is a request
+    response.send("Hello") //a response is when the API gives the info requested.
+});
+
+app.post('/login', (request, response)=>{ //A post is when a client sends new info to an API
+    const loginRequest = request.body;
+    if (loginRequest.userName=="bob@bob.com" && loginRequest.password=="B0bwashere!"){
+        response.status(200); //200 means OK
+        response.send("Welcome")
+    } else {
+        response.status(401); //401 means unauthorized
+        response.send("Unauthorized")
+    }
+});
